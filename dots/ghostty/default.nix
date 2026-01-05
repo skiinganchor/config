aef69a06d96adce0ce1b6767e9cb37264e787ed6
skiinganchor <skiing_anchor.k4gaf@slmails.com>
2026-01-05 10:43:31 +0100

    Add ghostty terminal

---
+++ dots/ghostty/default.nix
@@ -0,0 +1,18 @@
+# note: currently do no support Linux, only Darwin
+# watch: https://github.com/ghostty-org/ghostty/issues/9606
+{ pkgs, ... }:
+{
+  programs.ghostty = {
+    enable = true;
+    package = pkgs.ghostty-bin;
+    settings = {
+      theme = "nord";
+      font-size = 16;
+      font-family = "Comic Code Ligatures";
+      command = "/run/current-system/sw/bin/tmux";
+      adjust-cell-height = "50%";
+      font-thicken = true;
+      font-thicken-strength = 120;
+    };
+  };
+}
