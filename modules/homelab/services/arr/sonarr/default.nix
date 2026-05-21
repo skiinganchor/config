7b4cfdbbd0464cf4c109b2732e4e6b2fb61a3530
skiinganchor <skiing_anchor.k4gaf@slmails.com>
2026-05-21 16:56:23 +0200

    Add OAuth2 config for *arr-stack

--- modules/homelab/services/arr/sonarr/default.nix
+++ modules/homelab/services/arr/sonarr/default.nix
@@ -53,8 +53,39 @@ in
           # Add X-XSS-Protection header for additional XSS protection
           add_header X-XSS-Protection "1; mode=block" always;
         '';
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:8989";
+        locations = {
+          "/" = {
+            extraConfig = ''
+              auth_request /oauth2/auth;
+              error_page 401 = /oauth2/sign_in;
+              auth_request_set $user  $upstream_http_x_auth_request_user;
+              auth_request_set $email $upstream_http_x_auth_request_email;
+              proxy_set_header X-User  $user;
+              proxy_set_header X-Email $email;
+              auth_request_set $auth_cookie $upstream_http_set_cookie;
+              add_header Set-Cookie $auth_cookie;
+              add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload" always;
+              add_header X-XSS-Protection "1; mode=block" always;
+
+              proxy_pass http://127.0.0.1:8989;
+            '';
+          };
+          "/oauth2/" = {
+            proxyPass = "http://127.0.0.1:4192";
+            extraConfig = ''
+              proxy_set_header X-Auth-Request-Redirect $request_uri;
+              proxy_buffer_size 16k;
+              proxy_buffers 4 32k;
+              proxy_busy_buffers_size 32k;
+            '';
+          };
+          "= /oauth2/auth" = {
+            proxyPass = "http://127.0.0.1:4192";
+            extraConfig = ''
+              proxy_set_header Content-Length "";
+              proxy_pass_request_body off;
+            '';
+          };
         };
         sslCertificate = "/var/lib/acme/${config.homelab.baseDomain}/fullchain.pem";
         sslCertificateKey = "/var/lib/acme/${config.homelab.baseDomain}/key.pem";
