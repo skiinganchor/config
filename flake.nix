50bd402d1c67b6e5acd8791c4a2778a41b557f64
skiinganchor <skiing_anchor.k4gaf@slmails.com>
2026-06-05 08:35:49 +0200

    Disable password auth in matrix-authentication and fix #2 nixvim input

--- flake.nix
+++ flake.nix
@@ -7,10 +7,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixvim.url = "github:nix-community/nixvim/nixos-26.05";
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
