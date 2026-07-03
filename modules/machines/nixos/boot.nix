a62d4a6a47238d27947d671cd93ed86fd1db5cf9
skiinganchor <skiing_anchor.k4gaf@slmails.com>
2026-07-03 08:15:03 +0200

    Add memtest86

--- modules/machines/nixos/boot.nix
+++ modules/machines/nixos/boot.nix
@@ -7,9 +7,9 @@
       efiSupport = true;
       efiInstallAsRemovable = true;
       useOSProber = true;
-      # efi.efiSysMountPoint = "/boot/efi";
       # Define on which hard drive you want to install Grub.
       device = "nodev"; # or "nodev" for efi only
+      memtest86.enable = true;
     };
     kernelPackages = pkgs.linuxPackages_latest;
   };
