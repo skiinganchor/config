ca3a02cb13c79b6d0c00a070933d52eedfba5b7b
skiinganchor <skiing_anchor.k4gaf@slmails.com>
2026-07-05 14:49:22 +0200

    Patched nvidia 580 driver

--- modules/machines/nixos/default.nix
+++ modules/machines/nixos/default.nix
@@ -1,6 +1,14 @@
 { config, lib, my-secrets, pkgs, sops-nix, ... }:
 let
   secretsPath = builtins.toString my-secrets;
+  nvidiaLegacy580Master = pkgs.pkgs-master.linuxPackages_latest.nvidiaPackages.legacy_580;
+  nvidiaLegacy580 = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    inherit (nvidiaLegacy580Master) version;
+    sha256_64bit = nvidiaLegacy580Master.src.outputHash;
+    openSha256 = nvidiaLegacy580Master.open.src.outputHash;
+    settingsSha256 = nvidiaLegacy580Master.settings.src.outputHash;
+    persistencedSha256 = nvidiaLegacy580Master.persistenced.src.outputHash;
+  };
 in
 {
   imports = [
@@ -22,7 +30,7 @@ in
     open = false;
     modesetting.enable = true; # wayland requirement
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
+    package = nvidiaLegacy580;
   };
 
   networking = {
