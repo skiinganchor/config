97cf69b5a169ef79504200db3adb1926c64dc6a6
skiinganchor <skiing_anchor.k4gaf@slmails.com>
2026-09-21 18:14:45 +0200

    fix(aurral): double import

--- flake.nix
+++ flake.nix
@@ -109,8 +109,6 @@
                 disko.nixosModules.disko
                 # TODO: Remove after upgrading nixpkgs to 26.11, where this module is included.
                 (import "${nixpkgs-master}/nixos/modules/services/matrix/matrix-authentication-service.nix")
-                # TODO: Remove after the nixpkgs pin includes NixOS/nixpkgs#562425.
-                (import "${nixpkgs-master}/nixos/modules/services/misc/aurral.nix")
                 (import "${self}/modules/machines/emilia")
               ]
               ++ defaultModules;
